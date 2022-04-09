fba0576290c03c9aca01c01bd1c5cf42b6fcf6ac
Vitor <vitorwlima13@gmail.com>
2022-04-09 20:20:16 -0300

    fix player order render

--- client/src/components/PlayerContainer/index.tsx
+++ client/src/components/PlayerContainer/index.tsx
@@ -6,16 +6,20 @@ type Props = {
   player: IPlayer
 }
 
-const PlayerContainer = ({ player }: Props) => {
-  const renderFaceDownCard = () => <div className='w-8 h-14 bg-zinc-900 rounded-sm' />
+const FaceDownCard = () => <div className='w-8 h-14 bg-zinc-900 rounded-sm' />
 
+const PlayerContainer = ({ player }: Props) => {
   return (
     <div className='bg-zinc-300 rounded-md w-32 p-2 text-zinc-900 flex flex-col items-center gap-y-2 text-lg'>
       <p className='font-bold'>
         {player.name}
         {player.isMe && ' (eu)'}
       </p>
-      <div className='flex gap-2'>{player.cards.map(_ => renderFaceDownCard())}</div>
+      <div className='flex gap-2'>
+        {player.cards.map((_, i) => (
+          <FaceDownCard key={i} />
+        ))}
+      </div>
       <div className='flex items-center gap-x-2'>
         <FaCoins />
         <p className='font-bold'>{player.coins}</p>
--- client/src/screens/Ingame/index.tsx
+++ client/src/screens/Ingame/index.tsx
@@ -3,11 +3,13 @@ import { useGameState } from 'src/hooks/useGameState'
 
 const Ingame = () => {
   const { gameState, myPlayer, emitEvent } = useGameState()
+  const playersInCorrectOrder = [...gameState.players].sort((a, b) => a.order - b.order)
+
   return (
     <div className='pt-10 pb-8 px-3'>
       <section className='flex gap-4 justify-center'>
-        {gameState.players.map(player => (
-          <PlayerContainer key={player.socketId} player={player} />
+        {playersInCorrectOrder.map(player => (
+          <PlayerContainer key={player.order} player={player} />
         ))}
       </section>
     </div>
