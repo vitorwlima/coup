0fc8549205a13df3155e6fb6a62f90a9174de0af
Vitor Windberg <vitorwlima13@gmail.com>
2024-04-16 16:26:59 -0300

    feat: update cards pic

--- client/src/components/PlayerInfo/index.tsx
+++ client/src/components/PlayerInfo/index.tsx
@@ -16,13 +16,13 @@ const PlayerInfo = ({ player }: Props) => {
       </div>
       <div className='flex gap-x-1'>
         {player.cards.map(card => (
-          <Image
-            key={card.id}
-            alt={card.value}
-            src={`/cards/${card.value}.jpeg`}
-            width={200}
-            height={300}
-          />
+          <div key={card.id} className='relative h-[300px] rounded-md overflow-hidden'>
+            <div className='absolute inset-0 z-10 bg-gradient-to-b from-zinc-900/0 to-zinc-900/90' />
+            <p className='capitalize absolute bottom-1 z-10 left-1/2 -translate-x-1/2 text-zinc-200 text-sm font-semibold'>
+              {card.value === 'capitao' ? 'capitão' : card.value}
+            </p>
+            <Image alt={card.value} src={`/cards/${card.value}.jpeg`} width={218} height={300} />
+          </div>
         ))}
       </div>
     </div>
